67f7e8ac3a7e71ff5b795b5d4c32b6505c4c1cf3
Your Name <Vaaax@Jon-PC>
2022-03-19 13:14:10 -0500

    created employee table

--- db/schema.sql
+++ db/schema.sql
@@ -6,4 +6,31 @@ USE employee_db;
 CREATE TABLE department (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
-)
+);
+
+CREATE TABLE roles (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL (10,2),
+    department_id INTEGER NOT NULL,
+    CONSTRAINT fk_department
+        FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    CONSTRAINT fk_role
+        FOREIGN KEY (role_id)
+        REFERENCES roles(id)
+        ON DELETE CASCADE
+    manager_id INTEGER NOT NULL,
+    CONSTRAINT fk_manager
+    FOREIGN KEY (manager_id)
+    REFERENCES manager(id)
+    ON DELETE SET NULL
+);
