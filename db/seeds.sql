df34434121b13dd1f4abd96f6e7518e782655e7c
Your Name <Vaaax@Jon-PC>
2022-03-19 13:51:19 -0500

    added values for roles tables

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (id, name)
+VALUES 
+    (1, 'Senior Software Engineer')
+    (2, 'Software Develop Engineer')    
+    (3, 'Intern Software Engineer')
+    (4, 'Full Stack Software Engineer')
+
+INSERT into roles (id, title, salary, department_id)
+VALUES 
+    (1, 'Marshall Trooper', 79999.99, 4),
+    (2, '501st Legion', 99999.99, 2),
+    (3, 'Clone Captain', 109999.99, 1),
+    (4, 'Jedi Knight', 9999999.99, 1),
+    (5, 'Padwan', 88888.99, 2),
+    (6, 'The Bad Batch', 100000.99, 4),
+    (7, 'Cadet Trooper', 77777.99, 3),
+    (8, 'Commander Trooper', 888888.99, 2),
+    (9, 'Shock Trooper', 99999.99, 4);
+
