932d5649497d59b8db2cc0bdca44c48149a9e0d4
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-04-15 23:21:29 -0500

    added employee table to seeds.sql and removed some roles in the roles table'

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,23 @@
 INSERT INTO department (id, name)
 VALUES 
-    (1, 'Senior Software Engineer')
-    (2, 'Software Develop Engineer')    
-    (3, 'Intern Software Engineer')
-    (4, 'Full Stack Software Engineer')
+    (1, 'Senior Software Engineer'),
+    (2, 'Software Develop Engineer'),    
+    (3, 'Intern Software Engineer'),
+    (4, 'Full Stack Software Engineer');
 
 INSERT into roles (id, title, salary, department_id)
 VALUES 
     (1, 'Marshall Trooper', 79999.99, 1),
     (2, '501st Legion Troopers', 99999.99, 2),
-    (3, 'Clone Captain Troopers', 109999.99, 1),
-    (4, 'Pilot Trooper', 9999999.99, 2),
-    (5, '187th Legion Troopers', 88888.99, 4),
-    (6, 'The Bad Batch', 100000.99, 4),
-    (7, 'Cadet Clone Trooper', 77777.99, 3),
-    (8, 'Commander Clone Trooper', 888888.99, 1),
-    (9, 'Shock Clone Trooper', 99999.99, 4);
-    (10, 'Bomb Squad Clone Trooper', 99999.99, 4);
+    (3, 'ARC Troopers', 109999.99, 1),
+    (4, 'The Bad Batch', 9999999.99, 2),
+    (5, '187th Legion Troopers', 88888.99, 4);
+
+INSERT into employee (first_name, last_name, role_id, manager_id)
+    ('Captain', 'Rex', 3, 1),
+    ('Fives', 'Trooper', 2, NULL),
+    ('Hunter', 'Trooper', 4, 1),
+    ('Echo', 'Trooper', 3, NULL),
+    ('Wrecker, Trooper', 4, NULL),
+    ('Commander', 'Cody', 1, 1),
+    ('Hevy', 'Trooper', 2, NULL)
